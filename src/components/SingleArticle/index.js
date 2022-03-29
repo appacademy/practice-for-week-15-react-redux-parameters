192fb4ba7572a66d10e256a1cb87a53dbba590ce
Edwin Carrillo <69633370+edwincarr@users.noreply.github.com>
2022-03-29 16:06:25 -0400

    initial commit

--- src/components/SingleArticle/index.js
+++ src/components/SingleArticle/index.js
@@ -1,19 +1,15 @@
-import './SingleArticle.css';
+import { useParams } from "react-router-dom";
+import "./SingleArticle.css";
 
-const SingleArticle = () => {
+const SingleArticle = ({ articles }) => {
+  const { id } = useParams("id");
+  const article = articles.find((art) => art.id === id);
+
   return (
-    <div className='singleArticle'>
-      <h1>Why Am I At Home</h1>
-      <img
-        src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg'
-        alt='home'
-      />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
-      </p>
+    <div className="singleArticle">
+      <h1>{article?.title}</h1>
+      <img src={article?.imageUrl} alt={article?.title} />
+      <p>{article?.body}</p>
     </div>
   );
 };
