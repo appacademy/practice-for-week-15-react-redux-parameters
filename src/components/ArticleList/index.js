80df0fa76e4cbba8e4f6263d0584fa8caeb3a0c4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-09-28 10:38:42 -0400

    initial commit

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,12 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Route, Switch, NavLink } from 'react-router-dom';
-import SingleArticle from '../SingleArticle';
-import { loadArticles } from '../../store/articleReducer';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, NavLink } from "react-router-dom";
+import SingleArticle from "../SingleArticle";
+import { loadArticles } from "../../store/articleReducer";
 
 const ArticleList = () => {
   const dispatch = useDispatch();
-  const articles = useSelector(state=>state.articleState.entries);
+  const articles = useSelector((state) => state.articleState.entries);
 
   useEffect(() => {
     dispatch(loadArticles());
@@ -17,13 +17,15 @@ const ArticleList = () => {
       <h1>Article List</h1>
       <ol>
         {articles.map(({ id, title }) => (
-          <li key={id}><NavLink to={`/article/${id}`}>{title}</NavLink></li>
+          <li key={id}>
+            <NavLink to={`/article/${id}`}>{title}</NavLink>
+          </li>
         ))}
       </ol>
 
       <Switch>
-        <Route path='/article/:id'>
-          <SingleArticle />
+        <Route path="/article/:id">
+          <SingleArticle articles={articles} />
         </Route>
       </Switch>
     </div>
