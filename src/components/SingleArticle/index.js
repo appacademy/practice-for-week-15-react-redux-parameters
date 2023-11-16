c4b95f33c27b6be61db8687683ff25281b8984eb
Nicholas Buscemi <112017671+nickbuscemi@users.noreply.github.com>
2023-11-16 15:58:12 -0500

    initial commit

--- src/components/SingleArticle/index.js
+++ src/components/SingleArticle/index.js
@@ -1,19 +1,23 @@
-import './SingleArticle.css';
+import "./SingleArticle.css";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const SingleArticle = () => {
+  const params = useParams();
+  const articleId = params.id;
+  const articles = useSelector((state) => state.articleState.entries);
+
+  const article = articles.find((article) => article.id === articleId);
+
+  if (!article) {
+    return <div>Article not found</div>;
+  }
+
   return (
-    <div className='singleArticle'>
-      <h1>Why Am I At Home</h1>
-      <img
-        src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg'
-        alt='home'
-      />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
-      </p>
+    <div className="singleArticle">
+      <h1>{article.title}</h1>
+      <img src={article.imageUrl} alt={article.title} />
+      <p>{article.body}</p>
     </div>
   );
 };
